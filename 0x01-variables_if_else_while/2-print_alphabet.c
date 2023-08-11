65b4e4bbcf645ef0a042627715247b20285fbd0e
mostafa <mostafalabib510@gmail.com>
2023-08-11 12:25:23 +0300

    print alphabet in order

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+    int s = 97;
+    int e = 123;
+    
+	while(s < e)
+        putchar(s++);
+
+	return (0);
+}
