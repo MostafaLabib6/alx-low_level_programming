9e383208fe7ea379f26a5dc8002422940a6a9958
mostafa <mostafalabib510@gmail.com>
2023-08-11 14:05:55 +0300

    t prints the lowercase alphabet in reverse,

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * 	main - print alphabet in reverse
+ * 
+ * Return: 0
+ * 
+*/
+
+int main(void)
+{
+	int s = 122;
+	int e = 96;
+	
+	while(s > e)
+		putchar(s--);
+	putchar('\n');
+
+	return (0);
+}
