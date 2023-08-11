e3c28fe290d69dcc611b15eb9aaf6f3e28da8040
mostafa <mostafalabib510@gmail.com>
2023-08-11 14:52:48 +0300

    update print alphabet file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,29 +1,26 @@
 #include<stdio.h>
-/*
-	main - print alphabets lower and upper case
 
-	Return: 0
-*/
+/**
+ *	main - print alphabets lower and upper case
+ *
+ * Return: Always 1.
+ */
 
 int main(void)
 {
-	int s = 97;
-	int e = 123;
+	int s = 97, e = 123;
 	
-	while(s < e){
-		if(s < 97)
+	while (s < e){
+		if (s < 97)
 			putchar(s++);
 
 		putchar(s++);
 	
-		if(s == 123){
+		if (s == 123){
 			s = 65;
 			e = 91;
-		}
-	
+		}	
 	}
 	putchar('\n');
-	
-
 	return (0);
 }
