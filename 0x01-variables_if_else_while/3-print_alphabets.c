095b6daed55f5e3bcb86e426d31e1c2eea100f76
mostafa <mostafalabib510@gmail.com>
2023-08-11 12:30:53 +0300

    print alphabat in order lower -upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int s = 97;
+    int e = 123;
+    
+	while(s < e){
+        if(s < 97)
+            putchar(s++);
+
+        putchar(s++);
+    
+        if(s == 123){
+            s = 65;
+            e = 91;
+        }
+    
+    }
+    
+
+	return (0);
+}
