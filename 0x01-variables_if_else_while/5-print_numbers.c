cb5fba54906310957132876e71b2a23195427614
mostafa <mostafalabib510@gmail.com>
2023-08-11 15:03:12 +0300

    update 5-print number file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,17 @@
 #include<stdio.h>
 
-
 /**
- * 	main - print number from 1 to 10 
- * 
- * Return 0
- * 
-*/
+ *	main - print number from 1 to 10
+ *
+ * Return: Always 1.
+ */
 
 int main(void)
 {
 	int n;
-	
-	for (n = 0; n < 10; n++){
+
+	for (n = 0; n < 10; n++)
 		printf("%d", n);
-	}
 	printf("\n");
-	
 	return (0);
 }
