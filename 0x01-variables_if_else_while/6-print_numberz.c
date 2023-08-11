483de5aad8041a47e8b8fa25b5201ea386d58f67
mostafa <mostafalabib510@gmail.com>
2023-08-11 15:04:33 +0300

    update print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,16 @@
 #include<stdio.h>
 
 /**
- * 	main - print number from 0 to 10 using putchar
+ *	main - print number from 0 to 10 using putchar
  * 
- * Return: 0
- * 
-*/
+ * Return: Always 1.
+ */
 int main(void)
 {
 	int num;
-	
+
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-
 	putchar('\n');
-
 	return (0);
 }
