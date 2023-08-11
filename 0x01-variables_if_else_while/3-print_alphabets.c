af2e3b0260612dd08a883a5710dd1bc5b04de683
mostafa <mostafalabib510@gmail.com>
2023-08-11 14:54:42 +0300

    update print alphabet file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int s = 97, e = 123;
 	
-	while (s < e){
+	while (s < e)
+	{
 		if (s < 97)
 			putchar(s++);
-
 		putchar(s++);
-	
-		if (s == 123){
+		if (s == 123)
+		{
 			s = 65;
 			e = 91;
-		}	
+		}
 	}
 	putchar('\n');
 	return (0);
