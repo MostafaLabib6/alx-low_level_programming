a9b1212999ed9289b4bc2699ef9d31e5233125d3
mostafa <mostafalabib510@gmail.com>
2023-08-16 01:29:38 +0300

    code that cause infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	printf("Infinite loop incoming :(\n");
+
+	num= 0;
+
+	/*while (num< 10)*/
+	/*{*/
+	/*putchar(num);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
