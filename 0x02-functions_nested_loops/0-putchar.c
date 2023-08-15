e438342fb83b1c9e1ffe8409c008d174b107a5d0
mostafa <mostafalabib510@gmail.com>
2023-08-15 13:36:36 +0300

    prints _putchar word.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
-#include<stdio.h>
-
+#include "main.h"
 /**
- *	main - print _putchar word
+ *	main - print _putchar word forward by a new line.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -11,7 +10,7 @@ int main(void)
 	int i;
 	char word[] = "_putchar";
 	for(i=0;word[i]!='\0';++i)
-		putchar(word[i]);
-	putchar('\n');
-	return 0;
+		_putchar(word[i]);
+	_putchar('\n');
+	return (0);
 }
