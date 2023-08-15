04620e21defd1447ded6ef0f9100619b7256a6f3
mostafa <mostafalabib510@gmail.com>
2023-08-15 13:44:59 +0300

    create 1-alphabet file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *	main - print the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; ++i)
+		_putchar(i + '0');
+	_putchar('\n');
+	return (0);
+}
