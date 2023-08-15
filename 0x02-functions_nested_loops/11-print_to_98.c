d6edb171da34647403df6b3dcc4596626ffe3845
mostafa <mostafalabib510@gmail.com>
2023-08-15 15:00:43 +0300

    print numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,9 +17,9 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
-			puts(", ");
+			printf(", ");
 		}
-		puts("\n");
+		printf("\n");
 	}
 	else
 	{
@@ -29,8 +29,8 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
-			puts(", ");
+			printf(", ");
 		}
-		puts("\n");
+		printf("\n");
 	}
 }
