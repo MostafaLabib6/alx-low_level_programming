97b262f3c4f8497af957eb6198c9bf5a37e256ce
mostafa <mostafalabib510@gmail.com>
2023-08-15 13:46:19 +0300

    update main.h file with alphabet prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *	main - print the alphabet, in lowercase, followed by a new line.
+ *	main - print the alphabet in lowercase, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
