5526a41b3e1f3ed2e1301e09066f55447f625c43
mostafa <mostafalabib510@gmail.com>
2023-08-15 14:03:42 +0300

    print alphabet 10 times.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,16 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void);
-
 void print_alphabet_x10(void)
 {
 	int i;
-	
+	char c;
+
 	for (i = 0; i < 10; ++i)
-		print_alphabet();
+	{
+		for (c = 'a'; c <= 'z'; ++c)
+			_putchar(c);
+		_putchar('\n');
+	}
+
 }
