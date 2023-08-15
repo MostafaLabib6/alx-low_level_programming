85fd2f640d6df0686f2329b49a12ff81b980306a
mostafa <mostafalabib510@gmail.com>
2023-08-15 14:12:09 +0300

    checks if a character is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is a letter.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
