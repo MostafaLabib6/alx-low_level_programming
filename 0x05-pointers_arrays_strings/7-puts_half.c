133d33d76c140892d80e249073e60a3ccf0c7569
mostafa <mostafalabib510@gmail.com>
2023-08-21 12:17:04 +0300

    add puts_half function

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,8 +12,15 @@ void puts_half(char *str)
 {
 	int len = strlen(str);
 	int i;
-
-	for (i = 0; i < len / 2; ++i)
-		_putchar(str[i]);
+	if (len % 2 ==0)
+	{
+		for (i = 0; i < len / 2; ++i)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (len - 1) / 2; i >= 0; ++i)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
