a91cd269b58071c6c1feaf68f6563e623ce749f7
mostafa <mostafalabib510@gmail.com>
2023-08-21 12:08:37 +0300

    print even in string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * puts2 - print every other character
+ * starting with the first character.
+ *
+ * @str: string parameter input
+ *
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+	int i;
+	int len = strlen(str);
+
+	for (i = 0; i < len; i+=2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
