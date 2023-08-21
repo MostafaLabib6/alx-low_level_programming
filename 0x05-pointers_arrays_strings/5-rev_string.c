7549d874dae2f415de54c2750037133f5e44c69b
mostafa <mostafalabib510@gmail.com>
2023-08-21 11:50:21 +0300

    update  rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,7 +3,7 @@
 
 /**
  * rev_string - reverse a string.
- * @s: array of char 
+ * @s: array of char
  * Return: void.
  */
 
@@ -13,9 +13,9 @@ void rev_string(char *s)
 	char temp;
 	int len = strlen(s);
 
-	for (i = 0; i < len/2; i++)
+	for (i = 0; i < len / 2; i++)
 	{
-		temp = s[i];  
+		temp = s[i];
 		s[i] = s[len - i - 1];
 		s[len - i - 1] = temp;
 	}
