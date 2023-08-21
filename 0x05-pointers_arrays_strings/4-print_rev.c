2bce27327d59f7c495e5df5d8c96ce04547d6240
mostafa <mostafalabib510@gmail.com>
2023-08-21 11:51:52 +0300

    update print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
 
 /**
  * print_rev - reverse a string.
- * @s: array of char 
+ * @s: array of char
  * Return: void.
  */
 
@@ -12,7 +12,7 @@ void print_rev(char *s)
 	int len = strlen(s) - 1;
 	int i;
 
-	for (i = len; i > 0; --i)
+	for (i = len; i >= 0; --i)
 		_putchar(s[i]);
 	_putchar('\n');
 }
