a9506bf98f1d2a7d2b300871e5a759a68781b006
mostafa <mostafalabib510@gmail.com>
2023-08-24 19:26:21 +0300

    update concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,5 +11,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	return (strcat (dest, src));
+	int c, c2;
+
+	c = 0;
+	while (dest[c])
+		c++;
+	for (c2 = 0; src[c2] ; c2++)
+		dest[c++] = src[c2];
+
+	return (dest);
 }
