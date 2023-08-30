02b802c17b9eefa273970e1adaeb298d08727780
mostafa <mostafalabib510@gmail.com>
2023-08-30 15:38:59 +0300

    fast power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,16 +1,25 @@
-
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - returns fast power
+ *
+ * @x: number
+ * @y: number
+ * Return: power of two number.
+*/
 
 
 int _pow_recursion(int x, int y)
 {
-    int ans;
-    
-    if (x == 0) return (0);
-    if (y == 0) return (1);
-    ans = _pow_recursion(x, y / 2);
-    ans = ans * ans;
-    if (y & 1)
-        return x * ans;
-    return ans;
+	int ans;
+	
+	if (y < 0) return (-1);
+	if (x == 0) return (0);
+	if (y == 0) return (1);
+	ans = _pow_recursion(x, y / 2);
+	ans = ans * ans;
+	if (y & 1)
+		return x * ans;
+	return ans;
 
 }
