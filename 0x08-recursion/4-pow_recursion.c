f53bf4aff949a6cdd0aaa0d80bef33d19ea186d2
mostafa <mostafalabib510@gmail.com>
2023-08-30 15:42:45 +0300

    fast power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -17,12 +17,12 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (x == 0)
 		return (0);
-	if (y == 0) 
+	if (y == 0)
 		return (1);
 	ans = _pow_recursion(x, y / 2);
 	ans = ans * ans;
 	if (y & 1)
-		return x * ans;
-	return ans;
+		return (x * ans);
+	return (ans);
 
 }
