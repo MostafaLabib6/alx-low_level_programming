62522b7220ec5e8b2b0f0d011d59cb5295816533
mostafa <mostafalabib510@gmail.com>
2023-09-04 22:49:11 +0300

    create w dimensional array of inegers with malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,26 +11,26 @@
 
 int **alloc_grid(int width, int height)
 {
-    int **arr;
-    int i,j,k;
+	int **arr;
+	int i, j, k;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
-    arr = (int **) malloc(height * sizeof(int *));
-    if (arr == NULL)
-        return (NULL);
-    for (i =0; i < width; i++ )
-    {
-        arr[i]=(int *) malloc(width * sizeof(int));
-        if (arr[i] == NULL)
-            return (NULL);
-    }
-    for (j =0; j < height; j++ )
-    {
-        for (k =0; k < width; k++ )
-        {   
-            arr[j][k] = 0;
-        }   
-    }
-    return (arr);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	arr = (int **) malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
+	for (i =0; i < width; i++ )
+	{
+		arr[i]=(int *) malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+			return (NULL);
+	}
+	for (j =0; j < height; j++ )
+	{
+		for (k =0; k < width; k++ )
+		{   
+			arr[j][k] = 0;
+		}   
+	}
+	return (arr);
 }
