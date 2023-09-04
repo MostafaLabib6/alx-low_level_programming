f20f2d4bc05ddcf3c27b586b16ff5d0b696a1357
mostafa <mostafalabib510@gmail.com>
2023-09-04 10:54:09 +0300

    create dublicate string using malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - create space in memory which contains a 
+ *		a compy of the string 
+ * @str:array of chars
+ *
+ * Return:NUll if str empty or fall to allocate,otherwise return string 
+ */
+
+
+
+char *_strdup(char *str)
+{
+	int len = strlen(str),i;
+	char *arr;
+
+	if (str == NULL)
+		return (str);
+	arr = malloc(len * sizeof(char));
+	if (arr == NULL)
+		return (arr);
+	for (i = 0; i < len; ++i)
+		arr[i] = str[i];
+	return arr;
+
+}
