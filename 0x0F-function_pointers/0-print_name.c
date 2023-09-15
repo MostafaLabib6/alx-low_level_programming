0d9a4807564de53a54f78af0d6b03f9035342c7b
mostafa <mostafalabib510@gmail.com>
2023-09-15 10:36:20 +0300

    update betty style

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,14 @@
-#include"function_pointers.h"
-
 /**
- * print_name - prints a name
+ * print_name - function to print name
  *
- * @name: array of characters
- * @f: function pointer
- * Return: Void
- */
+ * @name: name to print
+ * @f: function pointer that doesn't return anything
+ *
+ * Return: Always 0 (Success)
+*/
 
 void print_name(char *name, void (*f)(char *))
 {
-
 	if (name && f)
 		f(name);
-
 }
