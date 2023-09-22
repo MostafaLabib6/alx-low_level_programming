b6a1562e5139e9c09387b08abf3a7dc42a425fe3
mostafa <mostafa@example.com>
2023-09-22 18:18:33 +0300

    apply constractor attribute

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,19 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- *	myConstructor - Apply the constructor attribute to
- *	myConstructor() so that it
- *	is executed before main()
+ * myConstructor - Apply the constructor
+ * attribute to myConstructor() so that it
+ *   is executed before main()
  */
-
-void myConstructor(void) __attribute__((constructor));
+void myConstructor(void) __attribute__ ((constructor));
 
 /**
- *	myConstructor - Apply the constructor attribute to
- *	myConstructor() so that it
- *	is executed before main()
+ * myConstructor - implementation of myConstructor
  */
-
 void myConstructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
