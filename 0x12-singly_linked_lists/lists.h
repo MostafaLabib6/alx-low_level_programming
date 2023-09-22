d4b6763e2a28d8fc05067afce5947704febb3651
mostafa <mostafa@example.com>
2023-09-22 17:11:08 +0300

    update lists file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,10 @@
 
 #include <stddef.h>
 #include <stdio.h>
-#
+#include <stdlib.h>
+#include <string.h>
+
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -12,12 +15,14 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
     char *str;
     unsigned int len;
     struct list_s *next;
 } list_t;
+
 
 size_t print_list(const list_t *h);
 
