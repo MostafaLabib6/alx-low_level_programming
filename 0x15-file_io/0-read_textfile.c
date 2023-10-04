13a416c59d226f8701ff85598129c28e8ad7e8b2
mostafa <mostafa@example.com>
2023-10-04 13:42:02 +0300

    betty style update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,23 +1,24 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - read a text file and prints it.
- * @filename: filename
- * @letters: number of latters
- * Return: the actual number of bytes the function
- * read and print.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int r, o, w;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(letters * sizeof(char));
-
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
@@ -25,14 +26,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
-	if (o == -1 || r == -1 || w == -1)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
+
 	free(buffer);
-	close(0);
+	close(o);
 
 	return (w);
-
 }
